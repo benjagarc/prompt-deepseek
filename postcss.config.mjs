efc0a1ff5b3828369f3f803a6878b60d0a9dad16
bgarciabtk <bgarcia@btkfarma.com>
2025-04-15 14:15:47 -0600

    initialiaze configuraration to tawlwind and framer motion

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,4 +1,12 @@
 const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: ["@tailwindcss/postcss"],
 };
 
